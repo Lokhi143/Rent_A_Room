890d61bf6de580f32f0afe365f2a5ec12172d323
lokhi <lokhidas0792@gmail.com>
2017-08-29 18:31:55 +0530

    booking done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,32 @@
 class Booking < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :room
+	validate :booking_room
+
+
+	def booking_room
+		#binding.pry
+		bookings = Booking.where('room_id = ?', self.room_id)
+		new_start_date = self.start_date.to_datetime
+		new_end_date = self.end_date.to_datetime
+		future_dates = (new_start_date..new_end_date).to_a
+		
+		bookings.each do |booking|
+			#binding.pry
+			old_start_date = booking.start_date.to_datetime
+			old_end_date = booking.end_date.to_datetime
+			past_dates = (old_start_date..old_end_date).to_a
+			
+			if (self.end_date < Date.today) || (past_dates - future_dates).length != past_dates.length
+			
+				self.errors.add(:room_id, "Not available")
+				#binding.pry
+			end 
+		end
+
+
+
+	end	
+
+
 end
